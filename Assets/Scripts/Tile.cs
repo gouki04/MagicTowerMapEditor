7c995381c9cd5254da2b0d2a2aefc498e4a4a6d6
gouki04 <gouki04@126.com>
2019-06-13 23:13:25 +0800

    更新

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using MagicTower;
+
+
+public class Tile : MonoBehaviour
+{
+    public MagicTower.Logic.Tile.EType TileType;
+    public object[] Properties = null;
+}
--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -1,71 +1,62 @@
-using UnityEditor;
 using UnityEngine;
 
-namespace MagicTower.EditorData
+
+public class TileMap : MonoBehaviour
 {
-    public class Tile : MonoBehaviour
+    private int mWidth = 11;
+    public int Width
     {
-        public Logic.Tile.EType TileType;
-        public object[] Properties = null;
+        get { return mWidth; }
     }
 
-    public class TileMap : MonoBehaviour
+    private int mHeight = 11;
+    public int Height
     {
-        private int mWidth = 11;
-        public int Width
+        get { return mHeight; }
+    }
+
+    private GameObject[,] mLayerFloor;
+    private GameObject[,] mLayerCollide;
+
+    public void Init(int width, int height)
+    {
+        mWidth = width;
+        mHeight = height;
+
+        mLayerFloor = new GameObject[height, width];
+        mLayerCollide = new GameObject[height, width];
+    }
+
+    public void SetTile(int r, int c, GameObject tile)
+    {
+        var layer = tile.layer == 1 ? mLayerCollide : mLayerFloor;
+
+        var org_tile = layer[r, c];
+        if (org_tile != null)
         {
-            get { return mWidth; }
+            Destroy(org_tile);
         }
 
-        private int mHeight = 11;
-        public int Height
+        tile.transform.parent = transform;
+        tile.transform.localPosition = new Vector3(c, r, 0);
+
+        layer[r, c] = tile;
+    }
+
+    void OnDrawGizmos()
+    {
+        Vector3 pos = gameObject.transform.position;
+
+        for (float y = 0; y <= Height; ++y)
         {
-            get { return mHeight; }
+            Gizmos.DrawLine(new Vector3(pos.x - 0.5f, pos.y + y - 0.5f, 0.0f),
+                            new Vector3(pos.x - 0.5f + Width, pos.y + y - 0.5f, 0.0f));
         }
 
-        private GameObject[,] mLayerFloor;
-        private GameObject[,] mLayerCollide;
-
-        public void Init(int width, int height)
+        for (float x = 0; x <= Width; ++x)
         {
-            mWidth = width;
-            mHeight = height;
-
-            mLayerFloor = new GameObject[height, width];
-            mLayerCollide = new GameObject[height, width];
+            Gizmos.DrawLine(new Vector3(pos.x + x - 0.5f, pos.y - 0.5f, 0.0f),
+                            new Vector3(pos.x + x - 0.5f, pos.y + Height - 0.5f, 0.0f));
         }
-
-        public void SetTile(int r, int c, GameObject tile)
-        {
-            var layer = tile.layer == 1 ? mLayerCollide : mLayerFloor;
-
-            var org_tile = layer[r, c];
-            if (org_tile != null)
-            {
-                Destroy(org_tile);
-            }
-
-            tile.transform.parent = transform;
-            tile.transform.localPosition = new Vector3(c, r, 0);
-
-            layer[r, c] = tile;
-        }
-
-        void OnDrawGizmos()
-        {
-            Vector3 pos = gameObject.transform.position;
-
-            for (float y = 0; y <= Height; ++y)
-            {
-                Gizmos.DrawLine(new Vector3(pos.x - 0.5f, pos.y + y - 0.5f, 0.0f),
-                                new Vector3(pos.x - 0.5f + Width, pos.y + y - 0.5f, 0.0f));
-            }
-
-            for (float x = 0; x <= Width; ++x)
-            {
-                Gizmos.DrawLine(new Vector3(pos.x + x - 0.5f, pos.y - 0.5f, 0.0f),
-                                new Vector3(pos.x + x - 0.5f, pos.y + Height - 0.5f, 0.0f));
-            }
-        }
-    }  
+    }
 }
